a6c7283b0935c61f9edab3ab5d540ceabdacb409
ducndt-dgg <ducnd@dgg.network>
2024-02-21 17:19:36 +0700

    feat: add base redux-toolkip

--- src/pages/CollectionPage.jsx
+++ src/pages/CollectionPage.jsx
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useQueries } from "react-query";
 import { getListCountry } from "../apis/tutor-module";
+import { useAppDispatch } from "src/store";
+import { hideLoading, showLoading } from "src/store/features/loadingPage";
 
 function CollectionPage() {
   const { slug } = useParams();
   const [listCountry, setListCountry] = useState();
+  const dispatch = useAppDispatch();
 
   useQueries([
     {
@@ -23,10 +26,24 @@ function CollectionPage() {
   }, []);
 
   console.log("listCountry: ", listCountry);
+  const handleOpen = () => {
+    console.log("Test redux-tooltip line 30");
+    dispatch(showLoading());
 
+    const resetState = setInterval(() => {
+      dispatch(hideLoading());
+    }, 2000);
+    console.log("Test redux-tooltip line 37");
+    return () => clearInterval(resetState);
+  };
+  const handleClose = () => {
+    dispatch(hideLoading());
+  };
   return (
     <div>
       <div>CollectionPage {slug}</div>
+      <div onClick={handleOpen}>Click Open</div>
+      <div onClick={handleClose}>Click close</div>
     </div>
   );
 }
